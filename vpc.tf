247359dccca50525129e0e08ffa9f9bb4a3acd82
PAVITHRA-MNS <106266186+PAVITHRA-MNS@users.noreply.github.com>
2026-03-30 10:50:32 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "AppVPC" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "AppVPC"
+ }
+}
+
+resource "aws_subnet" "AppSubnet1" {
+  vpc_id     = aws_vpc.AppVPC.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "AppSubnet1"
+  }
+}
+
+resource "aws_subnet" "AppSubnet2" {
+  vpc_id            = aws_vpc.AppVPC.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "AppSubnet2"
+  }
+}
+
+resource "aws_network_interface" "nw-interface1" {
+  subnet_id = aws_subnet.AppSubnet1.id
+  security_groups = [aws_security_group.WebTrafficSG.id]
+  tags = {
+    Name        = "nw-interface1"
+  }  
+}
+
+resource "aws_network_interface" "nw-interface2" {
+  subnet_id = aws_subnet.AppSubnet2.id
+  security_groups = [aws_security_group.WebTrafficSG.id]
+  tags = {
+    Name        = "nw-interface2"
+  }  
+}
